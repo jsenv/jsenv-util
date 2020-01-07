00b52d2489ebde2019f13f828ec952a98a624142
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-07 14:58:06 +0100

    update writeParentDirectories test

--- test/writeParentDirectories/writeParentDirectories.test.js
+++ test/writeParentDirectories/writeParentDirectories.test.js
@@ -1,17 +1,36 @@
 import { assert } from "@jsenv/assert"
-import { resolveUrl, cleanDirectory, writeParentDirectories, directoryExists } from "../../index.js"
+import {
+  resolveUrl,
+  writeDirectory,
+  cleanDirectory,
+  writeParentDirectories,
+  testFileSystemNodePresence,
+} from "../../index.js"
 
-const directoryUrl = import.meta.resolve("./directory/")
-const subdirectoryUrl = resolveUrl("subdir/", directoryUrl)
-const fileUrl = resolveUrl("file.js", subdirectoryUrl)
-await cleanDirectory(directoryUrl)
-await writeParentDirectories(fileUrl)
+const tempDirectoryUrl = import.meta.resolve("./temp/")
+await cleanDirectory(tempDirectoryUrl)
 
+// destination parent does not exists
 {
-  const actual = await directoryExists(subdirectoryUrl)
+  const parentDirectoryUrl = resolveUrl("dir/", tempDirectoryUrl)
+  const destinationUrl = resolveUrl("dir/file.js", tempDirectoryUrl)
+
+  await writeParentDirectories(destinationUrl)
+  const actual = await testFileSystemNodePresence(parentDirectoryUrl)
   const expected = true
   assert({ actual, expected })
+  await cleanDirectory(tempDirectoryUrl)
 }
 
-// ensure does not throw if already exists
-await writeParentDirectories(fileUrl)
+// destination parent is a directory
+{
+  const parentDirectoryUrl = resolveUrl("dir/", tempDirectoryUrl)
+  const destinationUrl = resolveUrl("dir/file.js", tempDirectoryUrl)
+  await writeDirectory(parentDirectoryUrl)
+
+  await writeParentDirectories(destinationUrl)
+  const actual = await testFileSystemNodePresence(parentDirectoryUrl)
+  const expected = true
+  assert({ actual, expected })
+  await cleanDirectory(tempDirectoryUrl)
+}
