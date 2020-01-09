455189994432f86d3f06b35b2b940104b4678c41
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-09 11:12:23 +0100

    fix windows file url resolution

--- src/resolveUrl.js
+++ src/resolveUrl.js
@@ -1,6 +1,43 @@
+import { urlToFileSystemPath } from "./urlToFileSystemPath.js"
+
+const isWindows = process.platform === "win32"
+
+const baseUrlFallback = urlToFileSystemPath(process.cwd())
+
 export const resolveUrl = (specifier, baseUrl) => {
   if (typeof baseUrl === "undefined") {
-    throw new TypeError(`baseUrl missing`)
+    throw new TypeError(`baseUrl missing to resolve ${specifier}`)
   }
-  return String(new URL(specifier, baseUrl))
+
+  const url = String(new URL(specifier, baseUrl))
+  if (isWindows) {
+    /*
+    we want the following a special url resolution on windows regarding files:
+
+    resolveUrl("/file.js", "file:///C:/dir/file.js")
+    -> file:///C:/file.js
+
+    resolveUrl("file:///file.js", "file:///C:/dir/file.js")
+    -> file:///C:/file.js
+
+    resolveUrl("//file.js", "file:///C:/dir/file.js")
+    -> file:///C:/file.js
+
+    This is to be sure we don't loose the drive letter otherwise the url becomes invalid
+    */
+    if (url.startsWith("file:///")) {
+      const afterProtocol = url.slice("file:///".length)
+      // we still have the windows drive letter
+      if (afterProtocol[0].test(/[a-zA-Z]/) && afterProtocol[1] === ":") {
+        return url
+      }
+
+      // drive letter was lost, restore it
+      const driveLetter = (baseUrl.startsWith("file:///") ? baseUrl : baseUrlFallback).slice(
+        "file:///".length,
+      )[0]
+      return `file:///${driveLetter}:${afterProtocol}`
+    }
+  }
+  return url
 }
--- test/resolveUrl.test.js
+++ test/resolveUrl.test.js
@@ -7,10 +7,44 @@ import { resolveUrl } from "../index.js"
   assert({ actual, expected })
 }
 
-try {
-  resolveUrl("./foo.js")
-  throw new Error("should throw")
-} catch (actual) {
-  const expected = new TypeError(`baseUrl missing`)
-  assert({ actual, expected })
+{
+  const specifier = "./foo.js"
+
+  try {
+    resolveUrl(specifier)
+    throw new Error("should throw")
+  } catch (actual) {
+    const expected = new TypeError(`baseUrl missing to resolve ${specifier}`)
+    assert({ actual, expected })
+  }
+}
+
+if (process.platform === "win32") {
+  // sepcifier starting with /
+  {
+    const actual = resolveUrl("/file.js", "file:///C:/directory/file.js")
+    const expected = "file:///C:/file.js"
+    assert({ actual, expected })
+  }
+
+  // absolute file specifier
+  {
+    const actual = resolveUrl("file:///file.js", "file:///d:/directory/file.js")
+    const expected = "file:///d:/file.js"
+    assert({ actual, expected })
+  }
+
+  // scheme relative specifier
+  {
+    const actual = resolveUrl("//dir/file.js", "file:///C:/directory/file.js")
+    const expected = "file:///C:/dir/file.js"
+    assert({ actual, expected })
+  }
+
+  // baseUrl is not a file (fallback to process.cwd drive letter)
+  {
+    const actual = resolveUrl("file:///file.js", "http://example.com")
+    const expected = `file:///${process.cwd()[0]}:/file.js`
+    assert({ actual, expected })
+  }
 }
