728e8fcac7f8a8abaf5c0ba1d940ea0555aecd3c
Damien Maillard <dmaillard06@gmail.com>
2020-01-10 09:01:12 +0100

    fix failing test

--- test/writeFileSystemNodeModificationTime/writeFileSystemNodeModificationTime.test.js
+++ test/writeFileSystemNodeModificationTime/writeFileSystemNodeModificationTime.test.js
@@ -6,17 +6,18 @@ import {
   resolveUrl,
   writeFile,
 } from "../../index.js"
+import { toSecondsPrecision } from "../testHelpers.js"
 
 const tempDirectoryUrl = import.meta.resolve("./temp/")
 await ensureEmptyDirectory(tempDirectoryUrl)
 
 {
   const sourceUrl = resolveUrl("file.txt", tempDirectoryUrl)
-  const mtime = Date.now()
+  const mtime = toSecondsPrecision(Date.now())
   await writeFile(sourceUrl)
   await writeFileSystemNodeModificationTime(sourceUrl, mtime)
 
-  const actual = await readFileSystemNodeModificationTime(sourceUrl)
+  const actual = toSecondsPrecision(await readFileSystemNodeModificationTime(sourceUrl))
   const expected = mtime
   assert({ actual, expected })
 }
