df86ead3db1c8ac861e3e52898ad3d471ec4c71e
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-15 17:36:33 +0100

    update window eperm fix

--- src/internal/createWatcher.js
+++ src/internal/createWatcher.js
@@ -9,11 +9,16 @@ export const createWatcher = (sourcePath, options) => {
     watcher.on("error", async (error) => {
       // https://github.com/joyent/node/issues/4337
       if (error.code === "EPERM") {
-        const err = await new Promise((resolve, reject) => {
+        await new Promise((resolve, reject) => {
           open(sourcePath, "r", (openError, fd) => {
+            if (openError) {
+              console.error(`error while fixing windows eperm: ${openError.stack}`)
+            }
+
             if (fd) {
               close(fd, (closeError) => {
                 if (closeError) {
+                  console.error(`error while fixing windows eperm: ${closeError.stack}`)
                   reject(error)
                 } else {
                   resolve()
@@ -24,7 +29,8 @@ export const createWatcher = (sourcePath, options) => {
             }
           })
         })
-        console.error(err)
+      } else {
+        throw error
       }
     })
   }
