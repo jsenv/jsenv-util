0fcc3a7b29835a4b3fa6cced2dc10a1d930486af
Damien Maillard <damien.maillard@dailymotion.com>
2020-10-10 11:46:18 +0200

    add as parameter to readFile

--- src/readFile.js
+++ src/readFile.js
@@ -4,9 +4,18 @@ import { assertAndNormalizeFileUrl } from "./assertAndNormalizeFileUrl.js"
 import { urlToFileSystemPath } from "./urlToFileSystemPath.js"
 
 const readFilePromisified = promisify(readFileNode)
-export const readFile = async (value) => {
+export const readFile = async (value, { as = "string" } = {}) => {
   const fileUrl = assertAndNormalizeFileUrl(value)
   const filePath = urlToFileSystemPath(fileUrl)
   const buffer = await readFilePromisified(filePath)
-  return buffer.toString()
+  if (as === "buffer") {
+    return buffer
+  }
+  if (as === "string") {
+    return buffer.toString()
+  }
+  if (as === "json") {
+    return JSON.parse(buffer.toString())
+  }
+  throw new Error(`as must be one of buffer,string,json, received ${as}.`)
 }
