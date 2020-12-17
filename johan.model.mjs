67607cc6680d2b8f6170c3367f21f75b06b2f984
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-17 17:08:36 +0100

    delete forgotten files

--- johan.model.mjs
+++
@@ -1,8 +0,0 @@
-import { resolveUrl, readFile } from "@jsenv/util"
-
-const directoryUrl = resolveUrl("./", import.meta.url)
-console.log(directoryUrl)
-
-const packageUrl = resolveUrl("package.json", directoryUrl)
-const packageData = await readFile(packageUrl, { as: "json" })
-console.log(JSON.stringify(packageData, null, "  "))
